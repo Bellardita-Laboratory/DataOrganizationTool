77be8c7b60b5f5e14531e8c9d70380248a4bba01
Walid Outtaleb <walidouttaleb@live.fr>
2025-03-05 04:44:10 +0100

    Allow for multiple separators in StructureFinder

--- src/StructureFinder.py
+++ src/StructureFinder.py
@@ -6,55 +6,35 @@ import numpy as np
 from Levenshtein import distance
 
 from FileOrganizer import FileOrganizer
-
-# def combinations_idx(iterable, r):
-#     """
-#     Return successive r-length combinations of elements in the iterable, as well as the index of the last element used
-#     combinations('ABCD', 2) --> (AB,2) (AC, AD BC BD CD
-#     """
-#     # combinations('ABCD', 2) --> AB AC AD BC BD CD
-#     # combinations(range(4), 3) --> 012 013 023 123
-#     pool = tuple(iterable)
-#     n = len(pool)
-#     if r > n:
-#         return
-#     indices = list(range(r))
-#     yield tuple(pool[i] for i in indices), indices
-#     while True:
-#         for i in reversed(range(r)):
-#             if indices[i] != i + n - r:
-#                 break
-#         else:
-#             return
-#         indices[i] += 1
-#         for j in range(i+1, r):
-#             indices[j] = indices[j-1] + 1
-#         yield tuple(pool[i] for i in indices), indices
+from UI.UtilsUI import split_with_separators
 
 class StructureFinder:
-    def set_parameters(self, data_list:list[str], separator:str):
+    def set_parameters(self, data_list:list[str], separators:list[str]):
         self._data_list = data_list
-        self.separator = separator
+        self.set_separators(separators)
 
-        self._split_data = self._get_split_components()
+    def set_separators(self, separators:list[str]):
+        """
+            Set the separators to split the data into components and actualize the components limits (start, end) for each data
+        """
+        self.separators = separators
+        _, self._component_limits = self._get_split_components_ids()
 
-    def _get_split_components(self):
+    def _get_split_components_ids(self):
         """
             Split the data into components separated by the separator
         """
-        all_components = []
+        all_components : list[list[str]] = []
+        all_components_limits : list[list[tuple[int, int]]] = []
         for data in self._data_list:
-            components = data.split(self.separator)
-            all_components.append(components)
-        
-        return all_components
-    
-    def _get_all_possible_fusions(self, components:list[str], max_elem_left:int=0):
-        for start in range(len(components)):
-            for end in range(start, len(components)-max_elem_left):
-                yield (start, end, self.separator.join(components[start:end+1]))
+            components, component_limits = split_with_separators(data, self.separators)
 
-    def _get_possible_configurations(self, components:list[str], n_groups:int):
+            all_components.append(components)
+            all_components_limits.append(component_limits)
+        
+        return all_components, all_components_limits
+
+    def _get_possible_configurations(self, initial_str:str, component_limits:list[tuple[int,int]], n_groups:int):
         """
             Get all the possible configurations dividing the components into n_groups groups.
             Returns the possible configurations and the start and end index of the components used for each group.
@@ -68,26 +48,29 @@ class StructureFinder:
             return: [['A', 'B', 'C_D'], ['A', 'B_C', 'D'], ['A_B', 'C', 'D']], [[(0, 1), (1, 2), (2, 4)], [(0, 1), (1, 3), (3, 4)], [(0, 2), (2, 3), (3, 4)]]
         """
         if n_groups == 1:
-            return [[self.separator.join(components)]], [(0, len(components)-1)]
+            return [[initial_str]], [(0, len(component_limits))]
+        
 
         # From https://stackoverflow.com/questions/72063383/place-n-unique-elements-into-all-possible-k-groups-with-monotonicity-maintaining
         ans = []
         idx_ans = []
-        all_pos = list(range(1, len(components)))
+        all_pos = list(range(1, len(component_limits)))
         for pos in combinations(all_pos, n_groups - 1):
             tmp = []
             idx_tmp = []
             prev_idx = 0
             for curr_idx in pos:
-                tmp.append(self.separator.join(components[prev_idx:curr_idx]))
-                idx_tmp.append((prev_idx, curr_idx))
+                # tmp.append(self.separator.join(components[prev_idx:curr_idx]))
+                tmp.append(initial_str[component_limits[prev_idx][0]:component_limits[curr_idx-1][1]])
+                idx_tmp.append((prev_idx, curr_idx-1))
                 prev_idx = curr_idx
-            tmp.append(self.separator.join(components[curr_idx:]))
-            idx_tmp.append((curr_idx, len(components)))
+            # tmp.append(self.separator.join(components[curr_idx:]))
+            tmp.append(initial_str[component_limits[curr_idx][0]:])
+            idx_tmp.append((curr_idx, len(component_limits)))
 
             ans.append(tmp)
             idx_ans.append(idx_tmp)
-
+        
         return ans, idx_ans
     
     def get_structure(self, data_id:int, structure_id:int):
@@ -104,14 +87,19 @@ class StructureFinder:
                 _structure_data list that contains the possible values of the data for each component of the initial structure
                 _structure_idx list that contains the start and end index of the best match for each component of the initial structure
         """
-        components = self._split_data
+
+        n_groups = len(initial_structure)
         self._structure_data = [[] for _ in initial_structure]
         self._structure_idx = []
 
-        for line in components:
-            possible_configurations, idx = self._get_possible_configurations(line, len(initial_structure))
-            distances = [sum(distance(possible_configurations[i][j], initial_structure[j]) for j in range(len(initial_structure))) for i in range(len(possible_configurations))]
+        ## For each data, find the configuration that minimizes the distance between the initial structure and the data
+        for i,line in enumerate(self._data_list):
+            possible_configurations, idx = self._get_possible_configurations(line, self._component_limits[i], n_groups)
             
+            distances = [sum(distance(possible_configurations[i][j], initial_structure[j]) for j in range(n_groups)) 
+                         for i in range(len(possible_configurations))]
+
+            # Minimize the distance between the initial structure and the data
             best_config_id = np.argmin(distances)
             best_config = possible_configurations[best_config_id]
             
@@ -119,34 +107,8 @@ class StructureFinder:
                 self._structure_data[i].append(comp)
 
             self._structure_idx.append(idx[best_config_id])
-
-            # print("Initial structure: ", initial_structure)
-            # print("Best configuration: ", best_config)
-            # print("Best idx: ", idx[best_config_id])
-            # print()
-
-            # possible_fusions = []
-            # limits = []
-            # for start, end, fusion in self.get_all_possible_fusions(line):
-            #     possible_fusions.append(fusion)
-            #     limits.append((start, end))
-
-            # current_max_comp_id = 0
-            # for i, struct in enumerate(initial_structure):
-            #     distances = [distance(struct, fus) for fus in possible_fusions]
-            #     sorted_dist_ids = np.argsort(distances)
-                
-            #     for j in sorted_dist_ids:
-            #         start, end = limits[j]
-            #         if start >= current_max_comp_id:
-            #             structure_data[i].append(possible_fusions[j])
-            #             current_max_comp_id = end + 1
-
-            #             print(f'Best match: \t\t{possible_fusions[j]} from {start} to {end}')
-            #             print(f'Initial structure: \t{struct}')
-            #             print()
-            #             break
         
+        # Remove potential duplicates
         possible_structure_values = [set(comp) for comp in self._structure_data]
         self._sorted_possible_structure_values = [sorted(comp, reverse=True) for comp in possible_structure_values]
         return possible_structure_values
@@ -161,7 +123,7 @@ class StructureFinder:
             if i == 0:
                 structure_str += '('
             else:
-                structure_str += self.separator + '('
+                structure_str += '(' + '|'.join(self.separators) + ')('
             
             if i == batch_pos:
                 structure_str += batch_delimiter + ':'
@@ -194,8 +156,9 @@ if __name__ == "__main__":
     print(side_names)
 
     sf = StructureFinder()
-    sf.set_parameters(side_names, '_')
-    initial_structure = ['Dual_side_and_ventral', 'Mouse2Cage1', 'Post', 'MU_CX', 'Left', 'Run5DLC_resnet50_stxbp1_Corridor_SideviewOct3shuffle1_1000000_filtered']
+    sf.set_parameters(side_names, ['_', 'DLC'])
+    # initial_structure = ['Dual_side_and_ventral', 'Mouse2Cage1', 'Post', 'MU_CX', 'Left', 'Run5DLC_resnet50_stxbp1_Corridor_SideviewOct3shuffle1_1000000_filtered']
+    initial_structure = ['Dual_side_and_ventral_Mouse8Cage1_Post_WT_Left_Run6DLC_resnet50_stxbp1_Corridor', 'SideviewOct3shuffle1', '1000000_filtered']
     print(sf.find_structure(initial_structure))
     print(sf.get_structure(0, 0))
 
